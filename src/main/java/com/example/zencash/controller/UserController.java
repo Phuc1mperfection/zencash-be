9a9565f3703bd10674b480700134a7b5877e06de
Phuc1mperfection <phuc0200366@huce.edu.vn>
2025-04-05 18:44:46 +0700

    refactor: remove unused imports and thay đổi logic mật khẩu

--- src/main/java/com/example/zencash/controller/UserController.java
+++ src/main/java/com/example/zencash/controller/UserController.java
@@ -1,9 +1,9 @@
 package com.example.zencash.controller;
 
+import com.example.zencash.dto.ChangePasswordRequest;
 import com.example.zencash.dto.UpdateUserRequest;
 import com.example.zencash.dto.UserResponse;
 import com.example.zencash.exception.AppException;
-import com.example.zencash.entity.User;
 import com.example.zencash.repository.UserRepository;
 import com.example.zencash.service.UserService;
 import com.example.zencash.utils.ErrorCode;
@@ -15,20 +15,13 @@ import org.springframework.web.bind.annotation.*;
 
 import jakarta.validation.Valid;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
-    private final UserRepository userRepository;
-    private final PasswordEncoder passwordEncoder;
     private final UserService userService;
 
     public UserController(UserRepository userRepository, PasswordEncoder passwordEncoder, UserService userService) {
-        this.userRepository = userRepository;
-        this.passwordEncoder = passwordEncoder;
         this.userService = userService;
     }
     // Lấy thông tin user hiện tại
@@ -53,20 +46,17 @@ public class UserController {
     }
 
 
-    @PostMapping("/verify-password")
-    public ResponseEntity<Map<String, String>> verifyPassword(@AuthenticationPrincipal UserDetails userDetails,
-                                                              @RequestBody Map<String, String> request) {
-        User user = userRepository.findByEmail(userDetails.getUsername())
-                .orElseThrow(() -> new AppException(ErrorCode.INVALID_CREDENTIALS));
+    @PostMapping("/change-password")
+    public ResponseEntity<?> changePassword(
+            @RequestBody ChangePasswordRequest request,
+            @AuthenticationPrincipal UserDetails userDetails) {
 
-        String enteredPassword = request.get("password"); // Lấy mật khẩu user nhập
-        if (!passwordEncoder.matches(enteredPassword, user.getPassword())) {
-            throw new AppException(ErrorCode.INVALID_CREDENTIALS); // Sai mật khẩu
+        try {
+            userService.changePassword(userDetails.getUsername(), request);
+            return ResponseEntity.ok("Change password successfully!");
+        } catch (AppException e) {
+            return ResponseEntity.status(e.getHttpStatus())
+                    .body(e.getMessage()); // Return error message from the custom exception
         }
-
-        Map<String, String> response = new HashMap<>();
-        response.put("passwordRaw", enteredPassword);
-        return ResponseEntity.ok(response);
     }
-
 }
